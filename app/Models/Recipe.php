7587f2d2a9b4a70ef3cf0defab0b5a6acd9d686a
Jose Miguel <martinez97.pro@gmail.com>
2025-07-16 13:49:16 +0200

    [ADD] Models and migrations files that we have already executed

---
+++ app/Models/Recipe.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+
+    //protected $table = 'recipes'; //Nombre de la tabla PD: no es necesario definirlo porque laravel lo comprende, a no ser que no concuerden los nombres, es decir comprende que Recipe es el modelo de la table recipes pero si la tabla fuera recetas y el modelo recipes ya habria que aclararlo
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function recipeImages(){
+        return $this->hasMany(RecipeImage::class);
+    }
+
+    public function recipeSteps(){
+        return $this->hasMany(RecipeStep::class);
+    }
+
+    public function ingredients() {
+    return $this->belongsToMany(Ingredient::class, 'recipe_ingredients')->withPivot('quantity');
+}
+
+}
